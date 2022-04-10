9d82079c75b3bd92fc24097190819aa7b664bbbb
Kalefoo <kalefoo007@gmail.com>
2022-04-09 23:18:59 -0300

    IPLogger completed

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use((req, res) => {
+  (async() => {
+    const ipfy = await axios.get('https://api.ipify.org?format=json');
+    const ipv4 = ipfy.data
+
+    const info = await axios.get('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_ChH34CnEDykyF4zlcFgc7huesXwzE&ipAddress=' + ipv4.ip);
+    const data = info.data;
+
+    res.send(data);
+  });
+});
+
+app.listen(5000);
+
+(async() => {
+  const ipfy = await axios.get('https://api.ipify.org?format=json');
+  const ipv4 = ipfy.data
+  
+  console.log(ipv4)
+})
